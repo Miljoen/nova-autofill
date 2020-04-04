f511477ff513aa907d5b7d092c063c22f85bf823
Yoeri Moens <yoeri.moens@pixelindustries.com>
2020-04-04 15:33:04 +0200

    [Update] Rename NovaAutofill to Autofill

--- src/Autofill.php
+++ src/Autofill.php
@@ -5,14 +5,14 @@ namespace Miljoen\NovaAutofill;
 use Illuminate\Support\Collection;
 use Laravel\Nova\Fields\Field;
 
-class NovaAutofill extends Field
+class Autofill extends Field
 {
     /**
      * @var string
      */
     public $component = 'nova-autofill';
 
-    public function options(string $filterKey, Collection $options, Collection $models): NovaAutofill {
+    public function options(string $filterKey, Collection $options, Collection $models): Autofill {
         return $this->withMeta([
             'filterKey' => $filterKey,
             'options'   => $options,
