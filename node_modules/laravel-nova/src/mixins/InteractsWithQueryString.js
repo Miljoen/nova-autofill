721cc89fdd47ee0179025921adc7469d4262810c
Yoeri Moens <yoerimoens@gmail.com>
2020-05-23 11:30:18 +0200

    [Update] Adding all generated files required for using the package

---
+++ node_modules/laravel-nova/src/mixins/InteractsWithQueryString.js
@@ -0,0 +1,12 @@
+import defaults from 'lodash/defaults'
+
+export default {
+    methods: {
+        /**
+         * Update the given query string values.
+         */
+        updateQueryString(value) {
+            this.$router.push({ query: defaults(value, this.$route.query) })
+        },
+    },
+}
